d5c9a70ac00ecef96beddbb540787e114123e227
MaksIgnatchenko <foecunditate@gmail.com>
2018-05-27 20:42:56 +0300

    without file

--- index.php
+++ index.php
@@ -10,25 +10,20 @@ $mark = false;
 if (isset($_POST['code'])) {
     foreach($tests as $test) {
         $data = serialize($test[0]);
-        $preCode = '<?php';
-        $preCode .= ' $stream = fopen("php://stdin", "r");';
+        $interpreter = 'php -r ';
+        $preCode = ' $stream = fopen("php://stdin", "r");';
         $preCode .= ' $input = stream_get_contents($stream);';
         $preCode .= ' $args = unserialize($input);';
         $preCode .= ' $a = $args[0];';
         $preCode .= ' $b = $args[1];';
         $postCode = ' $result = solve($a, $b);';
         $postCode .= ' echo json_encode($result);';
-        $code = $preCode . $_POST['code'] . $postCode;
-        $filename = 'run.php';
-        $file = fopen($filename, "w");
-        fwrite($file, $code);
-        fclose($file);
+        $cmd = $interpreter . '\'' . $preCode . $_POST['code'] . $postCode . '\'';
         $descriptorspec = array(
             0 => array("pipe", "r"),  // stdin - канал, из которого дочерний процесс будет читать
             1 => array("pipe", "w"),  // stdout - канал, в который дочерний процесс будет записывать
             2 => array("pipe", "w") // stderr - файл для записи
         );
-        $cmd = "php " . $filename;
         $process = proc_open($cmd, $descriptorspec, $pipes, null, null);
         if (is_resource($process)) {
             fwrite($pipes[0], $data);
@@ -43,7 +38,6 @@ if (isset($_POST['code'])) {
         if (!($test[1][0] === $result)) {
             $mark = true;
         }
-        unlink($filename);
     }
     if ($mark) {
         echo "Неверно";
